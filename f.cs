19379735d3497f0f5a3a97cf952324e648810283
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2023-01-19 11:44:57 +0200

    Create f.cs

---
+++ f.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main()
+    {
+       int userAge = int.Parse(Console.ReadLine());
+	   double jupiterYears = 11.86;
+	   double jupiterAge = userAge*jupiterYears;
+	   int journeyToJupiter = 9;
+	   int newEarthAge = userAge + journeyToJupiter;
+	   double newJupiterAge = newEarthAge*jupiterYears;
+		
+	   Console.WriteLine(jupiterAge + " " + newEarthAge + " " + newJupiterAge);
+		
+		
+        
+        
+    }
+}
