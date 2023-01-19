71cf3ecadfff76ce3cba623630593d86a13c3bde
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2023-01-19 12:38:17 +0200

    Create 2.10.cs

---
+++ 2.10.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string f = Console.ReadLine();
+		string s = Console.ReadLine();
+		string t = Console.ReadLine();
+		
+		Console.WriteLine(t + s + f);
+			
+	}
+}
