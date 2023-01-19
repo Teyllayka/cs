9f5adb186aa648d4d7782d69fc60685d2cf2e11d
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2023-01-19 11:57:39 +0200

    Create fourth.cs

---
+++ fourth.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		double numberOne = 6.5;
+		int numberTwo = 4;
+		
+		Console.WriteLine(Math.Pow(numberOne, numberTwo) + " " + Math.Ceiling(numberOne) + " " + Math.Max(numberOne, numberTwo));
+	}
+}
