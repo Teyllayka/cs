06c1b7a6528ca710d4eaa40737dbeff4ef22819a
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2023-01-19 12:34:00 +0200

    Update s.cs

--- s.cs
+++ s.cs
@@ -5,7 +5,7 @@ public class Program
 	public static void Main()
 	{
 		int students = 18;
-		int groupSize = 5;
+		int groupSize = 4;
 		while (students % groupSize != 0 && groupSize > 2) {
 			Console.WriteLine("Unsuccessful, group size = " + groupSize);
 			groupSize--;
