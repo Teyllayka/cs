d56a3304f7b443f8a5b65960f15856274dd1e53f
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2023-03-16 11:26:54 +0200

    Create main.cs

---
+++ 7.6/main.cs
@@ -0,0 +1,54 @@
+using System;
+using Some =  dtm.dtm;
+using Some2 = cikl.cikl;
+using Some3 = str.str;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+      
+      Some a = new Some();
+      Some2 b = new Some2();
+      Some3 c = new Some3();
+      a.first();
+      Console.WriteLine("------------------");
+      
+      a.second();
+      Console.WriteLine("------------------");
+      
+      a.third();
+      Console.WriteLine("------------------");
+      
+      a.fourth();
+      
+      Console.WriteLine("------------------");
+
+      b.first();
+      Console.WriteLine("------------------");
+      
+      b.second();
+      Console.WriteLine("------------------");
+      
+      b.third();
+      Console.WriteLine("------------------");
+      
+      b.fourth();
+      
+      Console.WriteLine("------------------");
+
+      c.first();
+      Console.WriteLine("------------------");
+      
+      
+      c.second();
+      Console.WriteLine("------------------");
+      
+      c.third();
+      Console.WriteLine("------------------");
+      
+      c.fourth();
+
+    }
+}
